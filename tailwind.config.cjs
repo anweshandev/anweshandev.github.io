ea49166063f0bae3176e3cf7616ad3c852b117f9
Anwehan Roy Chowdhury <anweshanrc15@gmail.com>
2022-10-18 04:44:31 +0530

    :gear: Added configuration files.
    🌟 Added tailwindcss to project
    
    🎨 Project configuration also contains postcss.
    
    🌟 Added plugin daisyui to project

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,tsx,ts}',
+    './index.html',
+    // 'public/404.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
