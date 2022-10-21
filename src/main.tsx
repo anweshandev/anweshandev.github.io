a3e98dd41dec9cb3feaaaa0ad9ef0894d3d78d46
Anwehan Roy Chowdhury <anweshanrc15@gmail.com>
2022-10-22 05:27:59 +0530

    Modified components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import './index.css'
-// import App from '@/components/App'
+import App from '@/components/App/App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
   </React.StrictMode>
 )
+
+const protocol = import.meta.env.PROD ? 'https:' : 'http:';
+
+if (window.location.protocol === protocol) {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('/sw.js');
+	}	
+}
