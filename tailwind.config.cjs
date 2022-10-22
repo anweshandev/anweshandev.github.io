f1d688eabaa85c1b018489432179dab10ccad270
Anweshan Roy Chowdhury <anweshanrc15@gmail.com>
2022-10-22 19:27:34 +0530

    :gear: Modified

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,8 @@
 module.exports = {
   content: [
     './src/**/*.{js,jsx,tsx,ts}',
-    './index.html',
-    // 'public/404.html'
+    '**/*.html',
+    'public/sw.js'
   ],
   theme: {
     extend: {},
